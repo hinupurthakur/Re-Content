3344aad5b8f776bdd7a7eca8686e19d1f9757f4f
nupur-thakur <nupur.s.thakur@gmail.com>
2020-01-31 22:29:36 +0530

    Update extraction.py

--- Twitter/extraction.py
+++ Twitter/extraction.py
@@ -1,10 +1,10 @@
 import tweepy
 
 # Consumer keys and access tokens, used for OAuth
-consumer_key = 'wIMAr52yVySpGc7uZcdtjRMwT'
-consumer_secret = '1kI0l7Wn5yXW5W4s2RIYYzOUhpvvXyfqN1dfdGhqQfjAkgUG2z'
-access_token = '714460750173962240-pFSCIiTikGtorLHskgg48PL9Zjkc7yC'
-access_token_secret = '9lYsD14k1Sg1rZ7QLiD7CLRQ9fwNP26514d4ocJ3f9NFj'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
